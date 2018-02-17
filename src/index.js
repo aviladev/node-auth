ff9c2a1bcea086f5aeb350058fa84d3ce6978be9
Pedro Ávila <pedroavila95@gmail.com>
2018-02-17 13:43:04 -0200

    Add router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
 
+const router = require('./router')
+
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
 app.use(morgan('combined'))
 app.use(express.json())
+
+app.use('/', router)
 
 const server = http.createServer(app)
 
