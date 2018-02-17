9e4bf54347942443ece98b4cc7b84f9212dffd9c
Pedro Ávila <pedroavila95@gmail.com>
2018-02-17 19:27:56 -0200

    Add mongoose user model

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const router = require('./router')
+
+mongoose.connect('mongodb://localhost:27017/auth')
 
 const PORT = process.env.PORT || 8080
 
