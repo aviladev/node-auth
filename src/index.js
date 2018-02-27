e1727336cea68956560d7c89568292ee2bfc40c7
Pedro Ávila <pedroavila95@gmail.com>
2018-02-27 10:04:15 -0300

    Setup CORS

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ const PORT = process.env.PORT || 8080
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use(morgan('combined'))
 app.use(express.json())
 
