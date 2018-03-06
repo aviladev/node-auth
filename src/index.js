a628d04485066f40c726b5e189e231c3a103b444
Pedro Ávila <pedroavila95@gmail.com>
2018-03-06 09:51:08 -0300

    Allow authorization header

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, authorization')
   next()
 })
 
